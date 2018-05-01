3e8762c1e871d4b20f39478cbc1709ba3bcba2e0
Matt O <mo121@duke.edu>
2018-05-01 16:09:49 -0400

    webpack

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <p>Test</p>
+    <People :black_num="black_num"
+            :white_num="white_num">
+
+    </People>
+
+  </div>
+</template>
+
+
+
+<script>
+  import People from './components/People.vue'
+  
+  export default {
+  name: 'app',
+  components: {
+    People
+  },
+  data () {
+    return {
+      black_num: 5,
+      white_num: 3
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/People.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>{{black_num}}</p>
+    <p>{{white_num}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    props: ['black_num', 'white_num'],
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
